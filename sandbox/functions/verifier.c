2e20cb1951044aaa82782cfaf999dfe4fba22118
bpricemg <43038187+bpricemg@users.noreply.github.com>
2022-10-26 14:21:31 -0400

    Add files via upload

---
+++ sandbox/functions/verifier.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+
+// TODO: add verifier prototype
+
+int main(void)
+{
+    // input from user
+    int x = get_int("Enter the first int you are adding: ");
+    int y = get_int("Enter the second int you are adding: ");
+    int z = get_int("Enter the result: ");
+    
+    // TODO: verify the result using the function you wrote, and print 'correct!' or 'incorrect!'
+    
+}
+
+// TODO: write the verifier function
