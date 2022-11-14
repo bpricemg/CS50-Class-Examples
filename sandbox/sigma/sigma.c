f9a8ca07b6d6f9fd749f4199bd0def1266188bf1
bpricemg <43038187+bpricemg@users.noreply.github.com>
2022-11-14 08:47:11 -0500

    Create sigma.c

---
+++ sandbox/sigma/sigma.c
@@ -0,0 +1,22 @@
+/**
+ * Prints the sum of integers in the command line
+ *
+ * TODO: Write code that takes in command line arguments as ints (the function atoi() might 
+ * come in handy), and prints out their sum.
+ */
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+	// Input verification. If not proper input, print usage error and stop program
+    
+
+    // Read each of the arguments as ints and save to a running sum
+    
+
+    // Print the sum
+    
+
+    // Success
+}
