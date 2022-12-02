a4ae339fd4013051c81791e35181f399b82ae26e
bpricemg <43038187+bpricemg@users.noreply.github.com>
2022-12-02 09:45:46 -0500

    Create mobile.c

---
+++ sandbox/mobile/mobile.c
@@ -0,0 +1,29 @@
+#include <cs50.h>
+#include <stdio.h>
+
+// TODO: define the user_info struct below
+
+int main (void)
+{
+    // Test use of this struct
+    user_info test;
+    test.carrier = "AT&T";
+    test.name = "Brandon Price";
+    test.number = 4134589582;
+    test.incoming_mins = 134.2;
+    test.outgoing_mins = 256.1;
+    test.incoming_txts = 4050;
+    test.outgoing_txts = 2539;
+    
+    // Print test info
+    printf("User info is: \n");
+    printf("carrier: %s\n", test.carrier);
+    printf("name: %s\n", test.name);
+    printf("number: %li\n", test.number);
+    printf("incoming minutes: %.2f\n", test.incoming_mins);
+    printf("outgoing minutes: %.2f\n", test.outgoing_mins); 
+    printf("incoming text messages: %i\n", test.incoming_txts);
+    printf("outgoing text messages: %i\n", test.outgoing_txts);
+
+    return 0;
+}
