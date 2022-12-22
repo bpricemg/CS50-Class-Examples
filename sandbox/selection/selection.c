7c5825240428095da98a4b7b97dd0b089039357f
bpricemg <43038187+bpricemg@users.noreply.github.com>
2022-12-22 10:13:13 -0500

    Create selection.c

---
+++ sandbox/selection/selection.c
@@ -0,0 +1,43 @@
+// Selection Sort
+#include <cs50.h>
+#include <stdio.h>
+
+int main (void)
+{
+    // Get positive array size from user
+    int n;
+    do
+    {
+        n = get_int("Please enter array size: ");
+    }
+    while (n < 1);
+
+    // Populate array with integers from user
+    int array[n];
+    printf("Please enter %i elements to fill array: \n", n);
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = get_int();
+    }
+
+    // Display unsorted array
+    printf("unsorted array is: \n");
+    for(int i = 0; i < n; i++)
+    {
+        printf("%i ", array[i]);
+    }
+    printf("\n");
+
+    // TODO: Selection Sort
+
+
+    // Display sorted array
+    printf("sorted array is: \n");
+    for(int i = 0; i < n; i++)
+    {
+        printf("%i ", array[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
