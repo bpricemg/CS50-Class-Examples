2f569bc8ca96b9ea76a2d5b9eb562b5fafce4405
bpricemg <43038187+bpricemg@users.noreply.github.com>
2025-12-15 10:41:38 -0500

    Implement user input and prepare digitsum function
    Added user input for integer and a placeholder for recursive digitsum implementation.

--- sandbox/digitsum/digitsum.c
+++ sandbox/digitsum/digitsum.c
@@ -6,15 +6,20 @@ int digitsum(int n);
 
 int main(void)
 {
+    // Get an integer from a user
     int n = get_int("Integer: ");
+
+    // Print result with a function call of digitsum()
     printf("Sum of the digits: %d\n", digitsum(n));
 }
 
+// TODO: Recursive implementation for adding each digit of an integer
 int digitsum(int n)
 {
     // Base case
-    if (n < 10)
+    if (???)
         return ???;
+        
     // Recursive case
     else
         return ???;
